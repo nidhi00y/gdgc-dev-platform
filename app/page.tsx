67014043b4774331190b5bb654da14c13ef2c9f5
CodewithMukal <mukal.github@gmail.com>
2025-11-07 19:28:59 +0530

    feat(level 03): Fixed Responsiveness and Added Level 03 section

--- app/page.tsx
+++ app/page.tsx
@@ -1,118 +1,183 @@
 import AngularBrackets from "@/components/ui/angular-brackets";
 import Image from "next/image";
-import instagram from "./assets/instagram.svg"
-import twitter from "./assets/twitter.svg"
-import youtube from "./assets/youtube.svg"
-import linkedin from "./assets/linkedin.svg"
-import github from "./assets/github.svg"
+import instagram from "./assets/instagram.svg";
+import twitter from "./assets/twitter.svg";
+import youtube from "./assets/youtube.svg";
+import linkedin from "./assets/linkedin.svg";
+import github from "./assets/github.svg";
 import { Red_Hat_Mono, Space_Grotesk } from "next/font/google";
-import { Card } from "@/components/ui/card";
 import FameCard from "@/components/ui/fame-card";
-import hackmol from "./assets/hackmol.svg"
-import mol from "./assets/mol.svg"
-import gsc from "./assets/gsc.svg"
+import hackmol from "./assets/hackmol.svg";
+import purpleGrid from "./assets/purpleGrid.svg";
+import purpleGridLarge from "./assets/purpleGridLarge.svg";
+import { Card } from "@/components/ui/card";
+import Carasoul from "@/components/carasoul";
 
-const spaceGrotesk = Space_Grotesk({subsets:["latin"]})
-const redHatMono = Red_Hat_Mono({subsets: ["latin"]})
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+const redHatMono = Red_Hat_Mono({ subsets: ["latin"] });
 
 export default function Home() {
-  const dummyData = 
-  [
+  const colors: string[] = ["#FDD568", "#69A6FC", "#28D781", "#EA4335"];
+  const dummyData = [
     {
       title: "Hackmol",
       tag: "hackathon",
-      description: "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
-      color: "#FDD568",
-      image: hackmol
+      description:
+        "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
+      image: hackmol,
     },
     {
       title: "Hackmol",
       tag: "hackathon",
-      description: "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
-      color: "#69A6FC",
-      image: hackmol
+      description:
+        "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
+      image: hackmol,
     },
     {
       title: "Hackmol",
       tag: "hackathon",
-      description: "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
-      color: "#28D781",
-      image: hackmol
+      description:
+        "HackMOL is the flagship annual 30-hour hackathon being organized by Google Developer Student Club (GDSC) of NIT Jalandhar wherein young coders & developers from all over the country join together to build projects & solutions to the alarming problems of the region & the world.",
+      image: hackmol,
     },
-  ]
+  ];
+  const dummyDomains = ["DevOps","UI/UX","Web Dev","App Dev","AI/ML","WiT","CP"]
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex flex-col gap-5 font-sans">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <section className="flex flex-row justify-between">
-          <div className="flex flex-col justify-center items-center relative group">
+        <section className="flex px-[4%] lg:mt-[9%] flex-col md:flex-row justify-between my-[6%] gap-5">
+          <div className="flex flex-col justify-center items-center relative md:gap-6 sm:gap-2 lg:gap-8 group">
             <div className="relative flex justify-center items-center">
-              <div className="relative z-[5]">
-                <AngularBrackets color="#FFFFFF" />
+              <div className="relative z-[5] translate-1 group-hover:-translate-1 transition-transform duration-300">
+                <AngularBrackets className="" color="#FFFFFF" />
               </div>
-              <div className="absolute z-[4] translate-x-1 translate-y-1 group-hover:-translate-x-1 group-hover:-translate-y-1 transition-transform duration-300">
-                <AngularBrackets color="#EA4335" />
+              <div className="absolute z-[4] translate-2 group-hover:-translate-2 transition-transform duration-300">
+                <AngularBrackets className="" color="#EA4335" />
               </div>
-              <div className="absolute z-[3] translate-x-2 translate-y-2 group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300">
-                <AngularBrackets color="#34A853" />
+              <div className="absolute z-[3] translate-3 group-hover:-translate-3 transition-transform duration-300">
+                <AngularBrackets className="" color="#34A853" />
               </div>
-              <div className="absolute z-[2] translate-x-3 translate-y-3 group-hover:-translate-x-3 group-hover:-translate-y-3 transition-transform duration-300">
-                <AngularBrackets color="#FBBC04" />
+              <div className="absolute z-[2] translate-4 group-hover:-translate-4 transition-transform duration-300">
+                <AngularBrackets className="" color="#FBBC04" />
               </div>
-              <div className="absolute z-[1] translate-x-4 translate-y-4 group-hover:-translate-x-4 group-hover:-translate-y-4 transition-transform duration-300">
-                <AngularBrackets color="#4285F4" />
+              <div className="absolute z-[1] translate-5 group-hover:-translate-5 transition-transform duration-300">
+                <AngularBrackets className="" color="#4285F4" />
               </div>
             </div>
-            <div className={`flex relative text-8xl font-bold ${spaceGrotesk.className}`}>
-              <p className="relative text-white z-[5]">NITJ</p>
-              <p className="absolute text-[#EA4335] translate-x-[4px] translate-y-[4px] z-[4] group-hover:-translate-x-1 group-hover:-translate-y-[4px] transition-transform duration-300">
+            <div
+              className={`flex relative lg:text-8xl md:text-7xl text-6xl font-bold ${spaceGrotesk.className}`}
+            >
+              <p className="relative text-white z-[5] translate-1 group-hover:-translate-1 transition-transform duration-300">NITJ</p>
+              <p className="absolute text-[#EA4335] translate-2 z-[4] group-hover:-translate-2 transition-transform duration-300">
                 NITJ
               </p>
-              <p className="absolute text-[#34A853] translate-x-[8px] translate-y-[8px] z-[3] group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300">
+              <p className="absolute text-[#34A853] translate-3 z-[3] group-hover:-translate-3 transition-transform duration-300">
                 NITJ
               </p>
-              <p className="absolute text-[#FBBC04] translate-x-[12px] translate-y-[12px] z-[2] group-hover:-translate-x-3 group-hover:-translate-y-3 transition-transform duration-300">
+              <p className="absolute text-[#FBBC04] translate-4 z-[2] group-hover:-translate-4 transition-transform duration-300">
                 NITJ
               </p>
-              <p className="absolute text-[#4285F4] translate-x-[16px] translate-y-[16px] z-[1] group-hover:-translate-x-4 group-hover:-translate-y-4 transition-transform duration-300">
+              <p className="absolute text-[#4285F4] translate-5 z-[1] group-hover:-translate-5 transition-transform duration-300">
                 NITJ
               </p>
             </div>
           </div>
 
-          <div className="flex flex-col items-end text-right justify-center max-w-[40%] font-bold gap-[66px]">
-            <h1 className={`text-8xl ${spaceGrotesk.className}`}>
-              We are <span className="text-[#EA4335]">G</span><span className="text-[#34A853]">D</span><span className="text-[#FBBC04]">G</span><span className="text-[#6DA4FC]">C</span> NITJ
+          <div className="flex flex-col items-center text-center md:items-end md:text-right justify-center w-[90%] mx-auto md:mx-0 md:w-[50%] font-bold gap-[66px]">
+            <h1 className={`xl:text-8xl lg:text-7xl text-6xl ${spaceGrotesk.className}`}>
+              We are <span className="text-[#EA4335]">G</span>
+              <span className="text-[#34A853]">D</span>
+              <span className="text-[#FBBC04]">G</span>
+              <span className="text-[#6DA4FC]">C</span> <br /> NITJ
             </h1>
-            <p className={`text-2xl ${redHatMono.className}`}>
-              We're not just another events club. At GDG NITJ, we're building a <span className="text-[#FBBC04]">community</span> where everyone is both a learner and a teacher. Our <span className="text-[#EA4335]">goal</span> is to create a dynamic environment for collective <span className="text-[#34A853]">growth</span>. We bring this <span className="text-[#6DA4FC]">vision</span> to life through high-energy flagship events like DevJams, Hackmol, and WomenTechies, along with tons of insightful workshops to keep you ahead of the curve.
+            <p className={`lg:text-2xl md:text-xl text-lg ${redHatMono.className}`}>
+              We're not just another events club. At GDG NITJ, we're building a{" "}
+              <span className="text-[#FBBC04]">community</span> where everyone
+              is both a learner and a teacher. Our{" "}
+              <span className="text-[#EA4335]">goal</span> is to create a
+              dynamic environment for collective{" "}
+              <span className="text-[#34A853]">growth</span>. We bring this{" "}
+              <span className="text-[#6DA4FC]">vision</span> to life through
+              high-energy flagship events like DevJams, Hackmol, and
+              WomenTechies, along with tons of insightful workshops to keep you
+              ahead of the curve.
             </p>
             <div className="flex gap-[36px]">
-              <a href="https://www.instagram.com/gdgcnitj/" className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full p-3">
-                <Image src={instagram} width={40} height={40} alt="instagram"/>
+              <a
+                href="https://www.instagram.com/gdgcnitj/"
+                className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full"
+              >
+                <Image src={instagram} width={40} height={40} alt="instagram" />
               </a>
-              <a href="https://x.com/GDSCNitj" className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full p-3">
-                <Image src={twitter} width={40} height={40} alt="twitter"/>
+              <a
+                href="https://x.com/GDSCNitj"
+                className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full"
+              >
+                <Image src={twitter} width={40} height={40} alt="twitter" />
               </a>
-              <a href="" className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full p-3">
-                <Image src={linkedin} width={40} height={40} alt="linkedin"/>
+              <a
+                href=""
+                className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full"
+              >
+                <Image src={linkedin} width={40} height={40} alt="linkedin" />
               </a>
-              <a href="https://www.youtube.com/@DSCNITJ" className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full p-3">
-                <Image src={youtube} width={40} height={40} alt="youtube"/>
+              <a
+                href="https://www.youtube.com/@DSCNITJ"
+                className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full"
+              >
+                <Image src={youtube} width={40} height={40} alt="youtube" />
               </a>
-              <a href="https://github.com/gdgcnitj" className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full p-3">
-                <Image className="brightness-95 contrast-95 invert-33 sepia-7 saturate-184 hue-rotate-169" src={github} width={40} height={40} alt="github"/>
+              <a
+                href="https://github.com/gdgcnitj"
+                className="flex justify-center items-center hover:brightness-125 hover:bg-gray-400/10 rounded-full"
+              >
+                <Image
+                  className="brightness-95 contrast-95 invert-33 sepia-7 saturate-184 hue-rotate-169"
+                  src={github}
+                  width={40}
+                  height={40}
+                  alt="github"
+                />
               </a>
             </div>
           </div>
         </section>
-        <section className="my-20 flex w-full flex-col justify-center items-center">
-          <h1 className={`flex justify-center items-center font-bold ${spaceGrotesk.className} text-8xl`}>WALL OF FAME</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full mx-auto justify-center place-items-center py-21 md:flex-row">
-            {
-              dummyData.map((d,i)=>(
-                <FameCard title={d.title} color={d.color} description={d.description} tag={d.tag} image={d.image} key={i} />
-              ))
-            }
+        <section className="my-[1%] px-[4%] flex w-full flex-col justify-center items-center">
+          <h1
+            className={`flex justify-center items-center font-bold ${spaceGrotesk.className} lg:text-8xl md:text-7xl text-6xl text-center`}
+          >
+            WALL OF FAME
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full mx-auto justify-center place-items-center gap-20 py-21 md:flex-row">
+            {dummyData.map((d, i) => (
+              <FameCard
+                title={d.title}
+                color={colors[i % 4]}
+                description={d.description}
+                tag={d.tag}
+                image={d.image}
+                key={i}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="w-full relative h-[133vh] p-[4%] flex flex-col bg-cover" style={{backgroundImage: `url(${purpleGridLarge.src})` }}>
+          <div className="absolute w-full h-[33vh] z-0 bg-gradient-to-b from-background to-transparent top-0 left-0">
+          </div>
+          <div className="w-full flex justify-center items-center relative h-[33vh] z-10">
+            <h1 className={`text-center lg:text-8xl md:text-7xl text-6xl font-bold ${spaceGrotesk.className} z-10`}>LEVEL 03</h1>
+          </div>
+          <div className="relative flex flex-col justify-evenly items-center w-full h-[100vh]">
+            <div className="text-6xl relative group flex justify-center items-center text-center md:text-7xl lg:text-8xl font-bold">
+              <h1 className="relative z-50 translate-1 group-hover:-translate-1 transition-transform duration-300">Behind the Pixels</h1>
+              <h1 className="transition-transform duration-300 absolute text-[#FF0000] z-40 translate-2 group-hover:-translate-2">Behind the Pixels</h1>
+              <h1 className="transition-transform duration-300 absolute text-[#28D781] z-30 translate-3 group-hover:-translate-3">Behind the Pixels</h1>
+              <h1 className="transition-transform duration-300 absolute text-[#FDD568] z-20 translate-4 group-hover:-translate-4">Behind the Pixels</h1>
+              <h1 className="transition-transform duration-300 absolute text-[#69A6FC] z-10 translate-5 group-hover:-translate-5">Behind the Pixels</h1>
+            </div>
+            <div>
+              <Carasoul list={dummyDomains}/>
+            </div>
           </div>
         </section>
       </main>
--- components/ui/angular-brackets.tsx
+++ components/ui/angular-brackets.tsx
@@ -1,30 +1,33 @@
-import { NextPage } from 'next'
+import { NextPage } from "next";
+import { cn } from "@/lib/utils";
 
 interface Props {
-    color: string,
+  color: string;
+  className: string;
 }
 
-const AngularBrackets: NextPage<Props> = ({color}) => {
+const Side: NextPage<Props> = ({ className, color }) => {
   return (
-    <div className='flex gap-2'>
-        <div className=''>
-            <svg xmlns="http://www.w3.org/2000/svg" width="184" height="118" viewBox="0 0 184 118" fill="none">
-                <path d="M90.8789 117.342L165.362 72.0543C182.894 61.4006 188.877 37.7787 178.766 19.3042C168.656 0.82979 146.239 -5.4747 128.707 5.17903L0 83.4661L90.8789 117.342Z" fill={color}/>
-            </svg>
-            <svg className='relative bottom-10 right-[18px]' xmlns="http://www.w3.org/2000/svg" width="202" height="156" viewBox="0 0 202 156" fill="none">
-                <path d="M165.316 155.528C177.964 155.528 190.308 148.625 197.086 136.215C207.196 117.741 201.213 94.1195 183.681 83.4658L54.974 5.17902C37.4419 -5.47468 15.0252 0.829788 4.91489 19.3042C-5.19541 37.7786 0.787378 61.4004 18.3195 72.054L147.027 150.341C152.783 153.852 159.106 155.528 165.316 155.528Z" fill={color}/>
-            </svg>
-        </div>
-        <div className='rotate-y-180'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="184" height="118" viewBox="0 0 184 118" fill="none">
-                <path d="M90.8789 117.342L165.362 72.0543C182.894 61.4006 188.877 37.7787 178.766 19.3042C168.656 0.82979 146.239 -5.4747 128.707 5.17903L0 83.4661L90.8789 117.342Z" fill={color}/>
-            </svg>
-            <svg className='relative bottom-10 right-[18px]' xmlns="http://www.w3.org/2000/svg" width="202" height="156" viewBox="0 0 202 156" fill="none">
-                <path d="M165.316 155.528C177.964 155.528 190.308 148.625 197.086 136.215C207.196 117.741 201.213 94.1195 183.681 83.4658L54.974 5.17902C37.4419 -5.47468 15.0252 0.829788 4.91489 19.3042C-5.19541 37.7786 0.787378 61.4004 18.3195 72.054L147.027 150.341C152.783 153.852 159.106 155.528 165.316 155.528Z" fill={color}/>
-            </svg>
-        </div>
-    </div>
-)
-}
+    <div
+      style={{ backgroundColor: color }}
+      className={cn("w-[16vw] h-[5vw] max-w-[384px] max-h-[120px] rounded-full", className)}
+    ></div>
+  );
+};
 
-export default AngularBrackets
+const AngularBrackets: NextPage<Props> = ({ color }) => {
+  return (
+    <div className="flex flex-row gap-4 my-10">
+      <div className="flex flex-col rotate-z-180">
+        <Side className="rotate-27" color={color} />
+        <Side className="-rotate-27" color={color} />
+      </div>
+      <div className="flex flex-col">
+        <Side className="rotate-27" color={color} />
+        <Side className="-rotate-27" color={color} />
+      </div>
+    </div>
+  );
+};
+
+export default AngularBrackets;
